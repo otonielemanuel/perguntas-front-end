21173403dfd9984eb011882de94463d385c83be8
otonielemanuel <otonielemanuel21@gmail.com>
2023-05-04 21:07:29 +0100

    :sparkles: rotas

--- src/pages/error/index.tsx
+++ src/pages/error/index.tsx
@@ -1,9 +1,13 @@
 import * as styles from './styles'
+import { Link } from 'react-router-dom'
+import { BsArrowLeft } from 'react-icons/bs'
 
 export default function index() {
   return (
     <styles.Container>
-      <p>Erro 404!</p>
+      <h1>Erro 404!</h1>
+      <h3>Lamento Dev, a Página não foi encontrada.</h3>
+      <p><Link to="/"><BsArrowLeft size={10}/> Voltar pra casa</Link></p>
     </styles.Container>
   )
 }
--- src/pages/error/styles.ts
+++ src/pages/error/styles.ts
@@ -1,5 +1,23 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    color: ${props => props.theme.colors.white};
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  color: ${(props) => props.theme.colors.white};
+
+  h1 {
+    font-size: 3rem;
+  }
+
+  p {
+    font-size: 1rem;
+    a {
+        text-decoration: none;
+        color: ${(props) => props.theme.colors.white};
+    }
+  }
 `;
