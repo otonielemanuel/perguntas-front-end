098d4b489fea85c719392259c948b5bd9b95f4b0
otonielemanuel <otonielemanuel21@gmail.com>
2023-05-05 00:05:55 +0100

    :dizzy: commit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,14 +2,36 @@ import * as styles from './styles'
 import Nav from './Nav'
 import Logo from '../../assets/logo.svg'
 import { Link } from 'react-router-dom'
+import { useSpring, animated } from 'react-spring';
+import { useState } from 'react';
 
 export default function index() {
+  const [flip, setFlip] = useState(false);
+  const props = useSpring({
+    to: {
+      opacity: 1,
+      transform: 'translateY(0px)',
+    },
+    from: {
+      opacity: 0,
+      transform: 'translateY(10px)',
+    },
+    config: {
+      duration: 500,
+    },
+    reset: true,
+    reverse: flip,
+    delay: 500,
+    onRest: () => setFlip(flip),
+  })
   return (
     <styles.Header>
         <Link to="/">
           <styles.Image src={Logo} alt="Logotipo 120 perguntas" />
         </Link>
-        <Nav />
+        <animated.div style={props}>
+          <Nav />
+        </animated.div>
     </styles.Header>
   )
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,30 @@
 import Main from "../../components/mainHome"
+import { useSpring, animated } from 'react-spring';
+import { useState } from 'react';
+
 
 export default function index() {
+  const [flip, setFlip] = useState(false);
+  const props = useSpring({
+    to: {
+      opacity: 1,
+      transform: 'translateY(0px)',
+    },
+    from: {
+      opacity: 0,
+      transform: 'translateY(10px)',
+    },
+    config: {
+      duration: 500,
+    },
+    reset: true,
+    reverse: flip,
+    delay: 500,
+    onRest: () => setFlip(flip),
+  })
   return (
-    <>
+    <animated.div style={props}>
       <Main />
-    </>
+    </animated.div>
   )
 }
